b6ff3404ceb38a68dd07f4fb80bda7105b1542ca
hejabs <hendricjabs@gmail.com>
2015-08-08 18:33:11 +0200

    Dependencies changed and Rest call fixed

--- what2do/server/src/main/java/de/hsnr/abts/what2do/rest/resources/DataResource.java
+++ what2do/server/src/main/java/de/hsnr/abts/what2do/rest/resources/DataResource.java
@@ -1,29 +1,40 @@
-
 package de.hsnr.abts.what2do.rest.resources;
+
+import java.util.List;
+import java.util.logging.LogManager;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-/** Example resource class hosted at the URI path "/myresource"
+import org.slf4j.Logger;
+
+import de.hsnr.abts.what2do.business.BusinessFactory;
+import de.hsnr.abts.what2do.business.User;
+import de.hsnr.abts.what2do.commons.rest.RestDefinition;
+
+/**
+ * Example resource class hosted at the URI path "/myresource"
  */
 @Path("/data")
 public class DataResource {
+	BusinessFactory factory;
 	public DataResource() {
+		factory = new BusinessFactory();
 		System.out.println("Neue Instanz von HelloResource: " + this);
 	}
 
+	@Path(RestDefinition.GET_DATA_USER_ALL)
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getPlainMessage() {
-		return "Herzlich willkommen!";
-	}
-
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String getHtmlMessage() {
-		return "<html><body><h1>Herzlich willkommen!<h1></body></html>";
+	public String getUsers() {
+		List<User> list = factory.getUsers();
+		StringBuilder builder = new StringBuilder();
+		for(User u : list) {
+			builder.append(u);
+		}
+		return builder.toString();
 	}
 
 	@GET
@@ -31,7 +42,7 @@ public class DataResource {
 	public String getXmlMessage() {
 		return "<?xml version=\"1.0\"?><hello>Herzlich willkommen!</hello>";
 	}
-	
+
 	@Path("/extended")
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
@@ -39,4 +50,3 @@ public class DataResource {
 		return "Herzlich willkommen! Und gute Unterhaltung.";
 	}
 }
-
